0fa2ed01165f24b7a552637c385bb62f5ed523b4
Matt Smith <matthew.j.smith2@outlook.com>
2020-10-28 11:32:52 -0700

    Working with nested routes

--- react/reactshop/src/Routes.tsx
+++ react/reactshop/src/Routes.tsx
@@ -3,35 +3,58 @@ import {
   BrowserRouter as Router,
   Redirect,
   Route,
+  RouteComponentProps,
   Switch,
 } from "react-router-dom";
+import { CSSTransition, TransitionGroup, Transition } from 'react-transition-group';
 import Header from "./Header";
 
-import AdminPage from "./AdminPage";
 import ProductsPage from "./ProductsPage";
 import ProductPage from "./ProductPage";
 import NotFoundPage from "./NotFoundPage";
 import LoginPage from "./LoginPage";
+import { Suspense } from "react";
 
-const Routes: React.FunctionComponent = () => {
-  const [loggedIn, setLoggedIn] = React.useState(false);
+const AdminPage = React.lazy(() => import("./AdminPage"));
+
+const RoutesWrap: React.FunctionComponent = () => {
   return (
     <Router>
-      <div>
-        <Header />
-        <Switch>
-          <Redirect exact={true} from="/" to="/products" />
-          <Route exact={true} path="/products" component={ProductsPage} />
-          <Route path="/products/:id" component={ProductPage} />
-          <Route path="/admin">
-            {loggedIn ? <AdminPage /> : <Redirect to="/login" />}
-          </Route>
-          <Route path="/login" component={LoginPage} />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </div>
+      <Route component={Routes} />
     </Router>
   );
 };
 
-export default Routes;
+const Routes: React.FunctionComponent<RouteComponentProps> = props => {
+  const [loggedIn, setLoggedIn] = React.useState(true);
+  return (
+    <div>
+      <Header />
+      <TransitionGroup>
+        <CSSTransition
+          key={props.location.key}
+          timeout={500}
+          classNames="animate"
+        >
+          <Switch>
+            <Redirect exact={true} from="/" to="/products" />
+            <Route exact={true} path="/products" component={ProductsPage} />
+            <Route path="/products/:id" component={ProductPage} />
+            <Route path="/admin">
+              {loggedIn ?
+                <Suspense fallback={<div className="page-container">Loading...</div>}>
+                  <AdminPage />
+                </Suspense>
+                : <Redirect to="/login" />}
+            </Route>
+
+            <Route path="/login" component={LoginPage} />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </CSSTransition>
+      </TransitionGroup>
+    </div>
+  );
+};
+
+export default RoutesWrap;
