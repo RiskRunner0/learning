2b00c5a743110179f19311d4842d9a4c95c232d2
Matt Smith <matthew.j.smith2@outlook.com>
2022-04-27 19:13:20 -0700

    Through Ch9

---
+++ go/src/go_pl/ch9/bank2/bank1.go
@@ -0,0 +1,19 @@
+// Package bank provides a concurrency-safe bank with one account.
+package bank
+
+var (
+	sema    = make(chan struct{}, 1)
+	balance int
+)
+
+func Deposit(amount int) {
+	sema <- struct{}{}
+	balance = balance + amount
+	<-sema
+}
+func Balance() int {
+	sema <- struct{}{}
+	b := balance
+	<-sema
+	return b
+}
---
+++ go/src/go_pl/ch9/bank3/bank1.go
@@ -0,0 +1,21 @@
+// Package bank provides a concurrency-safe bank with one account.
+package bank
+
+import "sync"
+
+var (
+	mu      sync.Mutex
+	balance int
+)
+
+func Deposit(amount int) {
+	mu.Lock()
+	balance = balance + amount
+	mu.Unlock()
+}
+func Balance() int {
+	mu.Lock()
+	b := balance
+	mu.Unlock()
+	return b
+}
